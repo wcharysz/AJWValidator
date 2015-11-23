9302c8154c8513592624867e70d8e236381fc5fe
Wojciech Charysz <wojciech.charysz@hmmh.de>
2015-11-23 12:22:02 +0100

    Added Patterns Rules: ContainsCapital, PostalAddress, URL, PhoneNumber

--- Validator/Validator/Rules/ValidationRulePattern.swift
+++ Validator/Validator/Rules/ValidationRulePattern.swift
@@ -32,6 +32,11 @@ import Foundation
 public enum ValidationPattern: String {
     case EmailAddress = "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-+]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z‌​]{2,4})$"
     case ContainsNumber = ".*\\d.*"
+    case ContainsCapital = "^.*?[A-Z].*?$"
+    case PostalAddress = "[a-zA-Z\\d\\s\\-\\,\\#\\.\\+]+"
+    case URL = "_^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$_iuS"
+    case PhoneNumber = "\\+?\\(?\\d{2,4}\\)?[\\d\\s-]{3,}"
+
 }
 
 public struct ValidationRulePattern: ValidationRule {
